46e0e17d343ed2d4712a7a13847f195bd42fd6fc
Marian Dumitrascu <md@dumitrascu.net>
2020-05-25 15:01:23 +0000

    deletions

--- aws-examples/advanced_functionality/distributed_tensorflow_mask_rcnn/container-optimized/build_tools/set_env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export IMAGE_NAME=mask-rcnn-tensorflow-sagemaker
-export IMAGE_TAG=tf1.13-153442b
--- aws-examples/advanced_functionality/distributed_tensorflow_mask_rcnn/container/build_tools/set_env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export IMAGE_NAME=mask-rcnn-tensorpack-sagemaker
-export IMAGE_TAG=tf1.13-tp26664c3
